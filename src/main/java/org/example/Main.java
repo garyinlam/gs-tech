2027b0d5336ede637590b704e51495e17548dda5
Gar-Yin Lam <garyin.lam@gmail.com>
2023-02-20 10:18:43 +0000

    q2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static int medianMerged(int[] array1, int[] array2){
+        return 0;
+    }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
+    @Test
+    void medianMerged_ValidInput_ReturnCorrect(){
+        assertEquals(3, Main.medianMerged(new int[] {1,2,3}, new int[] {4,5}));
+    }
 }
